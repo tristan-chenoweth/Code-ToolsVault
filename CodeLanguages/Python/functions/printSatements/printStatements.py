ad6fb459189f4548df13ec65ca1f30ecb78aa61d
Tristan Chenoweth <tristan.chenoweth@outlook.com>
2023-12-13 13:10:54 -0600

    Code Vault Automatic backup: 2023-12-13 13:10:54
    Affected files:
    CodeLanguages/Python/functions/printSatements/printStatements.py

--- CodeLanguages/Python/functions/printSatements/printStatements.py
+++ CodeLanguages/Python/functions/printSatements/printStatements.py
@@ -1,3 +1,6 @@
+# handle imports
+import os
+
 # this file contains all useful print statement information for python
 
 text = "testing text"
@@ -19,3 +22,11 @@ print("This line has a \nline break")
 print("This line has a line break at the end that has been stripped before printing\n".rstrip())
 print("\nThis line has a line break at the begining that has been stripped before printing".lstrip())
 print("\nThis line has a line break at the begining and the end that has been stripped before printing\n".strip())
+
+# The following line calls a function from an import and prints to the terminal
+print(os.getlogin())
+
+# The following line uses concatination to print a message with a number
+
+print("This numebr is concatenated: " + str(49))
+
