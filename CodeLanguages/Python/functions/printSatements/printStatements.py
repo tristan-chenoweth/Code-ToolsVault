f17bdc2a09c7ff87d4689a9df9b4c8ae27f1ccc6
Tristan Chenoweth <tristan.chenoweth@outlook.com>
2023-12-13 11:30:54 -0600

    Code Vault Automatic backup: 2023-12-13 11:30:54
    Affected files:
    CodeLanguages/Python/functions/printSatements/printStatements.py

---
+++ CodeLanguages/Python/functions/printSatements/printStatements.py
@@ -0,0 +1,16 @@
+# this file contains all useful print statement information for python
+
+text = "testing text"
+number = 8
+array = [1,2,3]
+
+# The following line prints a plain 
+print("Printing plain text")
+
+# The following line prints a variable storing text
+# It can also be use to print numbers and arrays
+print(text)
+print(number)
+print(array)
+
+
