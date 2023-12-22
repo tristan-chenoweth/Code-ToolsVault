59f1a1ca83df47e5913d1a38c69ea08d1fe6af50
Tristan Chenoweth <tristan.chenoweth@outlook.com>
2023-12-22 12:36:39 -0600

    Code Vault Automatic backup: 2023-12-22 12:36:39
    Affected files:
    CodeLanguages/Java/UniversityJavaFiles/computerProgramming2/assignment_5/mersenne_primes.java

--- CodeLanguages/Java/UniversityJavaFiles/computerProgramming2/assignment_5/mersenne_primes.java
+++ CodeLanguages/Java/UniversityJavaFiles/computerProgramming2/assignment_5/mersenne_primes.java
@@ -23,14 +23,15 @@ public class mersenne_primes
 
 		for(BigInteger i = BigInteger.ZERO; i.compareTo(p) <= 0; i = i.add(BigInteger.ONE))
 		{
-			if(i.isProbablePrime(90) == true)
+			if(i.isProbablePrime(100) == true)
 			{
 				exponent = i.intValue();
 				BigInteger pow_base = new BigInteger("2");
 				BigInteger info = pow_base.pow(exponent).subtract(BigInteger.ONE);
-				if (info.isProbablePrime(90) == true)
+				if (info.isProbablePrime(100) == true)
 				{
 					System.out.println(i + "\t" + info);
+					System.out.println("");
 				}
 			}
 		}
