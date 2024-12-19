42b42e1ed6808ff9bf0585df81e76ee2392fae49
Tristan Chenoweth <tristan.chenoweth@outlook.com>
2024-12-18 20:14:03 -0600

    Code Vault Automatic backup: 2024-12-18 20:14:03
    Affected files:
    CodeLanguages/Python/functions/arithmeticOperators/arithmeticOperators.py

---
+++ CodeLanguages/Python/functions/arithmeticOperators/arithmeticOperators.py
@@ -0,0 +1,18 @@
+# 1. Arithmetic Operators
+
+print("=== Arithmetic Operators ===")
+
+# Variables
+
+a = 10
+b = 3
+
+# Addition
+print(f"{a} + {b} = {a + b}") # Output: 13
+
+# Subtraction
+print(f"{a} - {b} = {a - b}") # Output: 7
+
+# Multiplication
+print(f"{a} * {b} = {a * b}") # Output: 30
+
