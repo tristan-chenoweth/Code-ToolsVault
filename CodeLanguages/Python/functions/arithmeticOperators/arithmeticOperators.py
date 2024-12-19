674d3a0173a3ea9f552239dceed98dbbf3ee2399
Tristan Chenoweth <tristan.chenoweth@outlook.com>
2024-12-18 20:20:03 -0600

    Code Vault Automatic backup: 2024-12-18 20:20:03
    Affected files:
    CodeLanguages/Python/functions/arithmeticOperators/arithmeticOperators.py

--- CodeLanguages/Python/functions/arithmeticOperators/arithmeticOperators.py
+++ CodeLanguages/Python/functions/arithmeticOperators/arithmeticOperators.py
@@ -8,11 +8,35 @@ a = 10
 b = 3
 
 # Addition
+print("Addition")
 print(f"{a} + {b} = {a + b}") # Output: 13
+print()
 
 # Subtraction
+print("Subtraction")
 print(f"{a} - {b} = {a - b}") # Output: 7
+print()
 
 # Multiplication
+print("Multiplication")
 print(f"{a} * {b} = {a * b}") # Output: 30
+print()
 
+# Division
+print("Division")
+print(f"{a} / {b} = {a / b}") # Output: 3.333....
+print()
+
+# Floor Division
+print("Floor Division")
+print(f"{a} // {b} = {a // b}") # Output: 3
+print()
+
+# Modulus (Remainder)
+print("Modulus (Remainder)")
+print(f"{a} % {b} = {a % b}") # Output: 1
+print()
+
+# Exponentiation
+print("Exponentiation")
+print(f"{a} ** {b} = {a ** b}") # Output: 1000
