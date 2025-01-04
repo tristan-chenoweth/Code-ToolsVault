4e2a4ab2b26a2a60efc2e986b8e388f49a3c6fb0
Tristan Chenoweth <tristan.chenoweth@outlook.com>
2025-01-03 22:16:10 -0600

    Code Vault Automatic backup: 2025-01-03 22:16:10
    Affected files:
    CodeLanguages/Java/UniversityJavaFiles/computerProgramming2/assignment_5/big_pi.java

--- CodeLanguages/Java/UniversityJavaFiles/computerProgramming2/assignment_5/big_pi.java
+++ CodeLanguages/Java/UniversityJavaFiles/computerProgramming2/assignment_5/big_pi.java
@@ -19,15 +19,15 @@ public class big_pi
 		System.out.println("Enter number of decimal places: ");
 		decimal_places = input.nextInt();
 
-		BigDecimal calc;
+		BigDecimal calc = BigDecimal.valueOf(0);
 
-		for (int i = 0; i < iterations.intValue; i++)
+		for (int i = 0; i < iterations.intValue(); i++)
 		{
 			calc = calc.add(PIbig);
 		}
 
 		pi_ = calc.divide((BigDecimal)iterations);
 
-		System.out.println("Estimate: " + pi_.setScale(decimal_places, BigDecimal.ROUND_HALF_DOWN));
+		System.out.println("Estimate: " + pi_.setScale(decimal_places)/* .setScale(decimal_places, RoundingMode.HALF_DOWN)*/);
 	}
 }
